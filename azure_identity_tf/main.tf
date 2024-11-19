27bcdb4e3e8eec0073471dd3b431732e41a016b0
hkaanturgut <kaan.turgut@deop.ca>
2024-11-19 17:58:13 -0500

    adding new ad code

--- azure_identity_tf/main.tf
+++ azure_identity_tf/main.tf
@@ -1,32 +1,41 @@
-# data about the current subscription
-data "azurerm_subscription" "current" {}
 
-# create an app registration
+# Data about the current subscriptions
+data "azurerm_subscription" "current" {
+  for_each        = var.subscriptions
+  subscription_id = each.key
+}
+
+
+# create an app registration for each subscription
 resource "azuread_application" "hcp_terraform" {
-  display_name = "spn-${var.stack_name}"
+  for_each     = var.subscriptions
+  display_name = "spn-${var.stack_name}-${each.value}"
 }
 
-# create a service principal for the app
+# create a service principal for each app
 resource "azuread_service_principal" "hcp_terraform" {
-  client_id = azuread_application.hcp_terraform.client_id
+  for_each  = var.subscriptions
+  client_id = azuread_application.hcp_terraform[each.key].client_id
 }
 
-# assign the contributor role for the service principal
+# assign the contributor role for each service principal
 resource "azurerm_role_assignment" "contributor" {
-  scope                = data.azurerm_subscription.current.id
-  principal_id         = azuread_service_principal.hcp_terraform.object_id
+  for_each             = var.subscriptions
+  scope                = "/subscriptions/${each.key}"
+  principal_id         = azuread_service_principal.hcp_terraform[each.key].object_id
   role_definition_name = "Contributor"
 }
 
 # create federated identity credentials for **plan** operations
-# for each deployment name
+# for each deployment name and subscription
 resource "azuread_application_federated_identity_credential" "plan" {
-  for_each       = toset(var.deployment_names)
-  application_id = azuread_application.hcp_terraform.id
-  display_name   = "stack-deployment-${each.value}-plan"
+  for_each = { for sub, name in var.subscriptions : sub => name }
+
+  application_id = azuread_application.hcp_terraform[each.key].id
+  display_name   = "stack-${each.value}-plan"
   audiences      = ["api://AzureADTokenExchange"]
   issuer         = "https://app.terraform.io"
-  description    = "Plan operation for deployment '${each.value}'"
+  description    = "Plan operation for subscription '${each.value}'"
   subject = join(":", [
     "organization",
     var.organization_name,
@@ -34,7 +43,7 @@ resource "azuread_application_federated_identity_credential" "plan" {
     var.project_name,
     "stack",
     var.stack_name,
-    "deployment",
+    "subscription",
     each.value,
     "operation",
     "plan"
@@ -42,14 +51,15 @@ resource "azuread_application_federated_identity_credential" "plan" {
 }
 
 # create federated identity credentials for **apply** operations
-# for each deployment name
+# for each deployment name and subscription
 resource "azuread_application_federated_identity_credential" "apply" {
-  for_each       = toset(var.deployment_names)
-  application_id = azuread_application.hcp_terraform.id
-  display_name   = "stack-deployment-${each.value}-apply"
+  for_each = { for sub, name in var.subscriptions : sub => name }
+
+  application_id = azuread_application.hcp_terraform[each.key].id
+  display_name   = "stack-${each.value}-apply"
   audiences      = ["api://AzureADTokenExchange"]
   issuer         = "https://app.terraform.io"
-  description    = "Apply operation for deployment '${each.value}'"
+  description    = "Apply operation for subscription '${each.value}'"
   subject = join(":", [
     "organization",
     var.organization_name,
@@ -57,9 +67,10 @@ resource "azuread_application_federated_identity_credential" "apply" {
     var.project_name,
     "stack",
     var.stack_name,
-    "deployment",
+    "subscription",
     each.value,
     "operation",
     "apply"
   ])
 }
+
